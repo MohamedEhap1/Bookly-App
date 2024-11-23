361f207c2339160f87433cf456723e35307c17e5
Mohamed Ehab <mohamedehap172004@gmail.com>
2024-11-24 00:42:50 +0200

    add font family

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,5 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const test =
-      'https://th.bing.com/th/id/R.6988edcac8b4682ff03a1596f752fe38?rik=VhzBi0VMPDkhOQ&pid=ImgRaw&r=0';
+      'https://cdn.kobo.com/book-images/0e60ca66-8c2a-4429-a5ba-771c6898a13c/353/569/90/False/grave-intent-2.jpg';
 }
