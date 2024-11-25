86626d52ba659d606a2522a6f9f1927969314840
Mohamed Ehab <mohamedehap172004@gmail.com>
2024-11-26 00:51:25 +0200

    search view done part 2

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_view_result.dart';
 import 'package:flutter/material.dart';
 import 'custom_search_text_field.dart';
 
@@ -27,24 +28,6 @@ class SearchViewBody extends StatelessWidget {
           Expanded(child: SearchResultListView()),
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 9.0),
-          child: BookListViewItem(),
-        );
-      },
     );
   }
 }
