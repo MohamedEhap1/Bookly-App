397a56f7aca9c97c54b26dd3b451e8c31eba5000
Mohamed Ehab <mohamedehap172004@gmail.com>
2024-11-28 03:10:00 +0200

    create repo method and create error file

--- lib/features/home/data/models/repos/home_repo.dart
+++ lib/features/home/data/models/repos/home_repo.dart
@@ -1,5 +1,9 @@
 //repository pattern => abstract class detect what this feature will do but not how
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
 }
